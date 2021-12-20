2a1fe1ed45a67c5dac6d2b8ad2e825c76148a803
Cesar863 <90540881+Cesar863@users.noreply.github.com>
2021-12-20 18:16:10 -0500

    added working stats and inventory commands

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,5 +1,8 @@
-//const { jest } = require('@jest/globals');
-//const { expect } = require('@jest/globals');
+// const { jest } = require('@jest/globals');
+// const { expect } = require('@jest/globals');
+// const { test } = require('picomatch');
+
+
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 
@@ -18,4 +21,23 @@ test('create a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test("get players stats as an object", () => {
+    const player = new Player ("Dave");
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
